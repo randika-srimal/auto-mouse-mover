62d6089f32a770be83e639dae957a9ba6e216a00
Randika Srimal <email2randika@gmail.com>
2020-03-19 19:14:34 +0530

    Add feature to switch between windows.

--- automousemover.sh
+++ automousemover.sh
@@ -5,29 +5,19 @@ Windows=()
 WindowClassesArray=()
 Windows=$(wmctrl -lx|awk '{print $3}')
 for Window in $Windows ; do WindowClassesArray+=($Window) ; done
-echo "Press any key to quit from auto mouse move";
-
-# while [ true ] ; do
-# read -t 3 -n 1
-# if [ $? = 0 ] ; then
-# exit ;
-# else
-# xdotool mousemove 500 500;
-# xdotool mousemove 700 500;
-# sleep 5; #Mouse pointer moving time interval
-# xdotool mousemove 700 500;
-# xdotool mousemove 500 500;
-# fi
-# done
-
+echo "Press any key to quit";
 while [ true ] ; do
-read -t 3 -n 1
+read -t 1 -n 1
 if [ $? = 0 ] ; then
 exit ;
 else
+xdotool mousemove 500 500;
+xdotool mousemove 700 500;
 rand=$[$RANDOM % ${#WindowClassesArray[@]}]
 wmctrl -x -a ${WindowClassesArray[$rand]}
-sleep 10; #Window changing time interval
+sleep 10; #Time interval
+xdotool mousemove 700 500;
+xdotool mousemove 500 500;
 rand=$[$RANDOM % ${#WindowClassesArray[@]}]
 wmctrl -x -a ${WindowClassesArray[$rand]}
 fi
